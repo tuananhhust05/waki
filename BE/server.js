6cadda45562ebc58db9323be2ed3ec62eb2815cc
tuananhhust05 <tuananhhust05@gmail.com>
2025-11-13 17:46:28 +0700

    update

--- BE/server.js
+++ BE/server.js
@@ -42,8 +42,8 @@ io.on('connection', (socket) => {
 
 //  Middleware
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 // Health check
 app.get('/health', (req, res) => {
